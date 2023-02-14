67b86f034b2969d82b9ac3d88d3639312e2a9361
borovinsms <120593749+borovinsms@users.noreply.github.com>
2023-02-14 13:50:29 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Main {
+
+    private static void saveGame(GameProgress gameProgress, String path) {
+        try {
+            File file = new File(path);
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+            try (FileOutputStream fileOutputStream = new FileOutputStream(file, false)) {
+                try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+                    objectOutputStream.writeObject(gameProgress);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void zipFiles(String path, String[] files) {
+        try {
+            try (FileOutputStream fileOutputStream = new FileOutputStream(path)) {
+                try (ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream)) {
+                    for (String filePath : files) {
+                        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+                            ZipEntry zipEntry = new ZipEntry(filePath);
+                            zipOutputStream.putNextEntry(zipEntry);
+                            int read = fileInputStream.read();
+                            zipOutputStream.write(read);
+                            zipOutputStream.closeEntry();
+                            File file = new File(filePath);
+                            if (file.delete()) {
+                                System.out.println("Файл \"" + filePath + "\" успешно заархивирован.");
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String homeDir = System.getProperty("user.home");
+        String[] files = {
+                homeDir + "/Games/GunRunner/savegames/save1.dat",
+                homeDir + "/Games/GunRunner/savegames/save2.dat",
+                homeDir + "/Games/GunRunner/savegames/save3.dat"
+        };
+        GameProgress gameProgress1 = new GameProgress(100, 0, 1, 0);
+        GameProgress gameProgress2 = new GameProgress(90, 1, 2, 2.5);
+        GameProgress gameProgress3 = new GameProgress(70, 3, 3, 8.7);
+        saveGame(gameProgress1, files[0]);
+        saveGame(gameProgress2, files[1]);
+        saveGame(gameProgress3, files[2]);
+        zipFiles(homeDir + "/Games/GunRunner/savegames/saves.zip", files);
+    }
+}
